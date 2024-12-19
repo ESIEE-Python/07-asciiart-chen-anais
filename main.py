39f2f1114c573ac7ac77f1e77f8928b7382f4bac
chen-anais <anais.chen@edu.esiee.fr>
2024-12-19 09:14:45 +0000

    Anaïs Chen - asciiart

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+"""
+contient deux fonctions secondaires qui retournent la liste de tuples 
+encodant une chaîne de caractères passée en argument 
+selon un algorithme itératif ou récurssif
+une fonction principale main qui appelle les deux fonctions secondaires 
+"""
 #### Imports et définition des variables globales
-
+import sys
+sys.setrecursionlimit(1500)
 
 #### Fonctions secondaires
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples encodant une chaîne de caractères passée en argument 
+    selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -13,35 +21,55 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
+    c=[s[0]]
+    o=[1]
+    for i in range(1,len(s)) :
+        if s[i] == s[i-1] :
+            o[-1] += 1
+        else :
+            c.append(s[i])
+            o.append(1)
+    final = []
+    for item in zip(c,o) :
+        final.append(item)
+    return final
 
-    return [ ]
 
-
-def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+def artcode_r(chaine):
+    """retourne la liste de tuples encodant une chaîne de caractères passée en argument 
+    selon un algorithme récursif
 
     Args:
-        s (str): la chaîne de caractères à encoder
+        chaine (str): la chaîne de caractères à encoder
 
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
     # votre code ici
 
     # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
+    if chaine== "":
 
-    return []
-    
+        return []
+    # recherche nombre de caractères identiques au premier
+    lettre = chaine[0]
+    occurence =1
+    for item in chaine[1:]:
+        if item == lettre:
+            occurence += 1
+        else:
+            return [(lettre,occurence)]+ artcode_r(chaine[occurence:])
+
+    return [(lettre,occurence)]
+    # appel récursif
 
 #### Fonction principale
 
 
 def main():
+    """
+    appelle les fonctions secondaires
+    """
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
